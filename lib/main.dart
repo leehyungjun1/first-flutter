76b32c56c0bca758d3c666ab8688698bdf1d48b7
leehyungjoon <unpobby@nate.com>
2022-04-27 11:40:17 +0900

    count add minus

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,70 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
 
+class MyAppState extends State<MyApp> {
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'First App',
       theme: ThemeData(
-        primarySwatch: Colors.pink,
+        primarySwatch: Colors.blue,
       ),
-      home: MyHome(),
-    );
-  }
-}
-
-class MyHome extends StatelessWidget {
-  const MyHome({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'IVENET',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 20.0,
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('You have pushed the button this many times : '),
+              Text(
+                '$counter',
+                style: Theme.of(context).textTheme.displaySmall,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FloatingActionButton(
+                      child: Icon(Icons.add),
+                      onPressed: () {
+                        setState(() {
+                          counter++;
+                          print("$counter");
+                        });
+                      }),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  FloatingActionButton(
+                      child: Icon(
+                        Icons.remove,
+                        size: 30.0,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          counter--;
+                          print("$counter");
+                        });
+                      }),
+                ],
+              )
+            ],
           ),
         ),
-        actions: [
-          Icon(Icons.shopping_cart),
-          SizedBox(
-            width: 10.0,
-          ),
-          Icon(Icons.search)
-        ],
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Text(
-              'Hello World',
-              style: TextStyle(
-                  color: Colors.amber,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-      ),
-      drawer: Drawer(),
     );
   }
 }
