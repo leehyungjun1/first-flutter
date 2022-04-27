354307df7ef6f9c47d8ad8ac4fefc1c6bf18a724
leehyungjoon <unpobby@nate.com>
2022-04-27 16:58:04 +0900

    dice game

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,166 @@
 import 'package:flutter/material.dart';
+import 'dice.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return MyAppState();
-  }
-}
-
-class MyAppState extends State<MyApp> {
-  int counter = 0;
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'First App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('You have pushed the button this many times : '),
-              Text(
-                '$counter',
-                style: Theme.of(context).textTheme.displaySmall,
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  FloatingActionButton(
-                      child: Icon(Icons.add),
-                      onPressed: () {
-                        setState(() {
-                          counter++;
-                          print("$counter");
-                        });
-                      }),
-                  SizedBox(
-                    width: 20.0,
-                  ),
-                  FloatingActionButton(
-                      child: Icon(
-                        Icons.remove,
-                        size: 30.0,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          counter--;
-                          print("$counter");
-                        });
-                      }),
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
+      title: 'Dice game',
+      home: LogIn(),
     );
   }
+}
+
+class LogIn extends StatefulWidget {
+  const LogIn({Key? key}) : super(key: key);
+
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  TextEditingController controller = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Log in'),
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search), onPressed: () {})
+        ],
+      ),
+      body: Builder(builder: (context) {
+        return GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 50)),
+                Center(
+                  child: Image(
+                    image: AssetImage('image/chef.gif'),
+                    width: 170.0,
+                    height: 190.0,
+                  ),
+                ),
+                Form(
+                    child: Theme(
+                        data: ThemeData(
+                            primaryColor: Colors.teal,
+                            inputDecorationTheme: InputDecorationTheme(
+                                labelStyle: TextStyle(
+                              color: Colors.teal,
+                              fontSize: 15.0,
+                            ))),
+                        child: Container(
+                          padding: EdgeInsets.all(40.0),
+                          child: Column(
+                            children: [
+                              TextField(
+                                // autofocus: true,
+                                controller: controller,
+                                decoration: InputDecoration(
+                                  labelText: 'Enter dice',
+                                ),
+                                keyboardType: TextInputType.emailAddress,
+                              ),
+                              TextField(
+                                controller: controller2,
+                                decoration: InputDecoration(
+                                  labelText: 'Enter Password',
+                                ),
+                                keyboardType: TextInputType.text,
+                                obscureText: true,
+                              ),
+                              SizedBox(
+                                height: 40.0,
+                              ),
+                              ButtonTheme(
+                                minWidth: 100 / 0,
+                                height: 50.0,
+                                buttonColor: Colors.orangeAccent,
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    if (controller.text == 'dice' &&
+                                        controller2.text == '1234') {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => Dice()));
+                                    } else if (controller.text == 'dice' &&
+                                        controller2.text != '1234') {
+                                      showSnackBar2(context);
+                                    } else if (controller.text != 'dice' &&
+                                        controller2.text == '1234') {
+                                      showSnackBar3(context);
+                                    } else {
+                                      showSnackBar(context);
+                                    }
+                                  },
+                                  child: Icon(
+                                    Icons.arrow_forward,
+                                    color: Colors.white,
+                                    size: 35.0,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        )))
+              ],
+            ),
+          ),
+        );
+      }),
+    );
+  }
+}
+
+void showSnackBar(BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        '로그인 정보를 다시 확인하세요',
+        textAlign: TextAlign.center,
+      ),
+      duration: Duration(seconds: 2),
+      backgroundColor: Colors.blue,
+    ),
+  );
+}
+
+void showSnackBar2(BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        '비밀번호가 일치하지 않습니다.',
+        textAlign: TextAlign.center,
+      ),
+      duration: Duration(seconds: 2),
+      backgroundColor: Colors.blue,
+    ),
+  );
+}
+
+void showSnackBar3(BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        '아이디를 다시 확인해 주세요.',
+        textAlign: TextAlign.center,
+      ),
+      duration: Duration(seconds: 2),
+      backgroundColor: Colors.blue,
+    ),
+  );
 }
